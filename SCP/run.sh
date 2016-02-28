bce928a552bcebe0ffc4913bdf87594969e2338c
Arno Moonens <x-006@hotmail.com>
2016-02-28 23:19:55 +0100

    Changed printing of filename to be equal to 'best-known.txt' format

--- SCP/run.sh
+++ SCP/run.sh
@@ -12,14 +12,17 @@ for a in "${algorithms[@]}"; do
     for re in "" "--re"; do
         for instance in "$2"/*
         do
-            # instance_name=$("$instance" | xargs -n 1 basename)
             y=${instance%.txt}
             cost=$(eval "$1 --seed $seed --instance $instance --output $output --$a $re")
+            filename=${y##*/}
+            length=${#filename}
+            first=$(echo ${filename:3:$length-4}  | awk '{print toupper($0)}')
+            second=${filename:$length-1:1}
             if [ -z $re ]
             then
-                echo "${y##*/} $cost" >> "$a.txt"
+                echo "$first.$second $cost" >> "$a.txt"
             else
-                echo "${y##*/} $cost" >> "$a+re.txt"
+                echo "$first.$second $cost" >> "$a+re.txt"
             fi
         done
     done
