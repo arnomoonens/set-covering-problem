f252d59f9b29fa9aa50eddef23fdf8042c37bec8
Arno Moonens <x-006@hotmail.com>
2016-02-29 12:32:31 +0100

    Added execution and gathering of results for exercise 1.2

--- SCP/run.sh
+++ SCP/run.sh
@@ -6,24 +6,47 @@ output="out.txt"
 # $1 is path of scp executable
 # $2 is path of folder with instances
 
-algorithms=("ch1" "ch2" "ch3" "ch4")
-
-for a in "${algorithms[@]}"; do
+#Exercise 1.1
+for a in "ch1" "ch2" "ch3" "ch4"; do
     for re in "" "--re"; do
         for instance in "$2"/*
         do
-            y=${instance%.txt}
+            y=${instance%.txt} # get part before .txt
             cost=$(eval "$1 --seed $seed --instance $instance --output $output --$a $re")
-            filename=${y##*/}
+            filename=${y##*/} # Only keep filename (remove the path)
             length=${#filename}
             first=$(echo ${filename:3:$length-4}  | awk '{print toupper($0)}')
             second=${filename:$length-1:1}
-            if [ -z $re ]
+            if [ -z $re ] # if empty string
             then
                 echo "$first.$second $cost" >> "$a.txt"
             else
                 echo "$first.$second $cost" >> "$a+re.txt"
             fi
+        done
+    done
+done
+
+
+#Exercise 1.2
+for a in "ch1" "ch4"; do
+    for re in "" "--re"; do
+        for imp_algo in "fi" "bi"; do
+            for instance in "$2"/*
+            do
+                y=${instance%.txt} # get part before .txt
+                cost=$(eval "$1 --seed $seed --instance $instance --output $output --$a $re --$imp_algo")
+                filename=${y##*/} # Only keep filename (remove the path)
+                length=${#filename}
+                first=$(echo ${filename:3:$length-4}  | awk '{print toupper($0)}')
+                second=${filename:$length-1:1}
+                if [ -z $re ] # if empty string
+                then
+                    echo "$first.$second $cost" >> "$a+$imp_algo.txt"
+                else
+                    echo "$first.$second $cost" >> "$a+re+$imp_algo.txt"
+                fi
+            done
         done
     done
 done
