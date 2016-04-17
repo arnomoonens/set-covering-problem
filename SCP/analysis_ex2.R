053bc089c179f071edbfea68931fe33adbc074d7
Arno Moonens <x-006@hotmail.com>
2016-04-17 13:06:20 +0200

    Added generation of qualified RTD plots

--- SCP/analysis_ex2.R
+++ SCP/analysis_ex2.R
@@ -25,8 +25,64 @@ solq.aco <- solutionquality(result.aco, filtered)
 cat("Config: ils; Average deviation: ", mean(solq.ils), "\n", sep="")
 cat("Config: aco; Average deviation: ", mean(solq.aco), "\n", sep="")
 
-plot(solq.ils, solq.aco, main="Scatterplot Example", xlab="Solution quality ILS", ylab="Solution quality ACO")
+plot(solq.ils, solq.aco, main="Correlation plot for ils and aco", xlab="Solution quality ILS", ylab="Solution quality ACO")
 model <- lm(solq.aco ~ solq.ils)
 abline(model, col = "red")
 
 cat("P value when using Wilcoxon test: ", wilcox.test(solq.ils, solq.aco, paired=TRUE)$p.value, "\n", sep="")
+
+
+cat("Part 2: qualified RTDs\n")
+algos <- c("ils", "aco")
+runs <- 25
+instances <- c("A.1", "B.1", "C.1", "D.1")
+part2.results <- list()
+trace.config <- read.csv(paste0(results.folder, "trace_config.csv"), row.names=1)
+for(algo in algos) {
+    part2.results[[algo]] <- list()
+    for(instance in instances) {
+        part2.results[[algo]][[instance]] <- list()
+        for(i in 1:runs) {
+            part2.results[[algo]][[instance]][[i]] <- read.csv(paste0(results.folder, algo, "/", instance, "/", i, ".csv"), header = FALSE)
+            colnames(part2.results[[algo]][[instance]][[i]]) <- c("time", "cost")
+        }
+    }
+}
+
+rtd <- function(results, instance, t, q) {
+    total <- 0
+    for(run in 1:runs) {
+        df <- results[[instance]][[run]]
+        before.time <- df[df$time <= t,]
+        total <- total + ifelse(nrow(before.time) == 0,0.0, (tail(before.time,n=1)$cost <= q))
+    }
+    total/runs
+}
+
+plot_qrtd <- function(algo, instance) {
+    min.time <- Inf
+    max.time <- 0
+    for(run in runs) {
+        df <- part2.results[[algo]][[instance]][[run]]
+        min.time <- min(min.time, head(df, n=1)$time)
+        max.time <- max(max.time, tail(df, n=1)$time)
+    }
+    time <- min.time * 0.5
+    stop.time <- max.time * 1.5
+    x <- list()
+    y <- list()
+    while(time <= stop.time) {
+        x <- c(x, time)
+        res <- rtd(part2.results[[algo]], instance, time, trace.config[instance,]$best*1.02)
+        y <- c(y, res)
+        time <- time * 1.05
+    }
+    plot(x,y, xlab="Run-time", ylab="P(solve)", main=paste0("Qualified run distribution for ", algo, " on instance ", instance))
+    lines(x,y)
+}
+
+for (algo in algos) {
+    for (instance in instances) {
+        plot_qrtd(algo, instance)
+    }
+}
